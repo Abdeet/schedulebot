5a4d68425dcd1b2fc78deb607dcd805bc905f920
Abdeet <55215876+Abdeet@users.noreply.github.com>
2020-09-08 09:22:08 -0400

    added basic ib functionality

--- bot.py
+++ bot.py
@@ -10,17 +10,24 @@ import re
 """
 {
     "user" : string,
+    "ib" : boolean
     "names" : {
         "1": string,
         "2": string,
         "3": string,
-        "4": string
+        "4": string,
+        *"5": string,
+        *"6": string,
+        *"7": string,
     }
     "links" : {
-        "1" : string,
-        "2" : string,
-        "3" : string,
-        "4" : string
+        "1": string,
+        "2": string,
+        "3": string,
+        "4": string,
+        *"5": string,
+        *"6": string,
+        *"7": string,
     }
 }
 """
@@ -67,7 +74,7 @@ def change_data(changed_list):
         data_txt.writelines(changed_list)
 
 def create_empty_object(user_mention):
-    return {"user" : user_mention, "names" : {"1" : "nothing", "2" : "nothing", "3" : "nothing", "4" : "nothing"}, "links" : {"1" : "fortnite.com", "2" : "epicgames.com", "3" : "bing.com", "4" : "netscape.com"}}
+    return {"user" : user_mention,"ib" : False, "names" : {"1" : "nothing", "2" : "nothing", "3" : "nothing", "4" : "nothing"}, "links" : {"1" : "fortnite.com", "2" : "epicgames.com", "3" : "bing.com", "4" : "netscape.com"}}
  
 def get_mentioned_roles(message):
     roles = re.findall(r"<@&[0-9]+>",message)
@@ -92,7 +99,6 @@ class MyClient(discord.Client):
         if str(self.user.id) not in mentions:
             yes = False
             for role in message.guild.get_member(self.user.id).roles:
-                print(str(role.id))
                 if str(role.id) in roles:
                     messagecapitalization = messagecapitalization.replace(f"<@&{str(role.id)}>","").replace(f"<@{str(role.id)}>","").replace(f"<@!{str(role.id)}>","")
                     themessage = themessage.replace(f"<@&{str(role.id)}>","").replace(f"<@{str(role.id)}>","").replace(f"<@!{str(role.id)}>","")
@@ -101,158 +107,173 @@ class MyClient(discord.Client):
             if not yes:
                 return
         userdata = get_user_data(get_data(),str(message.author.id))
-        print(userdata)
-        print(messagecapitalization)
+        ib = userdata['ib']
         messagecapitalization = [x.strip() for x in messagecapitalization.replace("<@!749979907282436166>","").replace("<@749979907282436166>","").replace("<@&749979907282436166>","").split("-")]
         themessage = [x.strip() for x in themessage.replace("<@!749979907282436166>","").replace("<@749979907282436166>","").replace("<@&749979907282436166>","").split("-")]
-        if themessage[0] == "create":
+        if "create" in themessage[0]:
             messagecapitalization.pop(0)
             for x in messagecapitalization:
                 userdata['names'][x[0]] = x[2:].split("@")[0].strip()
                 userdata['links'][x[0]] = x[2:].split("@")[1].strip()
+            if len(userdata['names']) > 4:
+                userdata['ib'] = True
+            else:
+                userdata['ib'] = False
             change_data(change_user_data(get_data(),userdata))
             await message.channel.send(f"Schedule created for {message.author.mention}")    
-        if themessage[0] == "modify":
+        if "modify" in themessage[0]:
             messagecapitalization.pop(0)
             for x in messagecapitalization:
                 userdata['names'][x[0]] = x[2:].split("@")[0].strip()
                 userdata['links'][x[0]] = x[2:].split("@")[1].strip()
+            if len(userdata['names']) > 4:
+                userdata['ib'] = True
+            else:
+                userdata['ib'] = False
             change_data(change_user_data(get_data(),userdata))
             await message.channel.send(f"Schedule modified for {message.author.mention}")
-        if themessage[0] == "delete":
+        if "delete" in themessage[0]:
             delete_user_data(get_data(),userdata)
             await message.channel.send(f"Schedule deleted for {message.author.mention}")
-        if themessage[0] == "list":
-            listembed = discord.Embed(title = f"**Schedule for {message.author.name}**", description = f"**Block 1:** [{userdata['names']['1']}]({userdata['links']['1']})\n**Block 2:** [{userdata['names']['2']}]({userdata['links']['2']})\n**Block 3:** [{userdata['names']['3']}]({userdata['links']['3']})\n**Block 4:** [{userdata['names']['4']}]({userdata['links']['4']})")
+        if "list" in themessage[0]:
+            if not ib:
+                listembed = discord.Embed(title = f"**Schedule for {message.author.name}**", description = f"**Block 1:** [{userdata['names']['1']}]({userdata['links']['1']})\n**Block 2:** [{userdata['names']['2']}]({userdata['links']['2']})\n**Block 3:** [{userdata['names']['3']}]({userdata['links']['3']})\n**Block 4:** [{userdata['names']['4']}]({userdata['links']['4']})")
+            elif ib:
+                listembed = discord.Embed(title = f"**Schedule for {message.author.name}**", description = f"**Block 1:** [{userdata['names']['1']}]({userdata['links']['1']})\n**Block 2:** [{userdata['names']['2']}]({userdata['links']['2']})\n**Block 3:** [{userdata['names']['3']}]({userdata['links']['3']})\n**Block 4:** [{userdata['names']['4']}]({userdata['links']['4']})\n**Block 5:** [{userdata['names']['5']}]({userdata['links']['5']})\n**Block 6:** [{userdata['names']['6']}]({userdata['links']['6']})\n**Block 7:** [{userdata['names']['7']}]({userdata['links']['7']})")
             await message.channel.send(embed=listembed)              
-        if themessage[0] == "now":
+        if "now" in themessage[0]:
             time = datetime.datetime.now()
             hour = int(time.strftime("%H"))
             minute = int(time.strftime("%M"))
             weekday = time.weekday()
             period = '0'
-            if weekday == 0:
-                if hour == 8:
-                    period = "1"
-                elif hour == 9:
-                    if minute <= 50:
+            if not ib:
+                if weekday == 0:
+                    if hour == 8:
                         period = "1"
-                    elif minute > 50:
+                    elif hour == 9:
+                        if minute <= 50:
+                            period = "1"
+                        elif minute > 50:
+                            period = "2"
+                    elif hour == 10:
                         period = "2"
-                elif hour == 10:
-                    period = "2"
-                elif hour == 11:
-                    if minute <= 20:
-                        period = "2"
-                    elif minute > 20:
-                        period = "1"
-                elif hour == 12:
-                    if minute == 0:
-                        period = "1"
-                    elif minute > 20:
+                    elif hour == 11:
+                        if minute <= 20:
+                            period = "2"
+                        elif minute > 20:
+                            period = "1"
+                    elif hour == 12:
+                        if minute == 0:
+                            period = "1"
+                        elif minute > 20:
+                            period = "3"
+                    elif hour == 13:
+                        if minute <= 5:
+                            period = "3"
+                        elif minute > 5 and minute <= 45:
+                            period = "4"
+                if weekday == 1:
+                    if hour == 8:
                         period = "3"
-                elif hour == 13:
-                    if minute <= 5:
-                        period = "3"
-                    elif minute > 5 and minute <= 45:
+                    elif hour == 9:
+                        if minute <= 50:
+                            period = "3"
+                        elif minute > 50:
+                            period = "4"
+                    elif hour == 10:
                         period = "4"
-            if weekday == 1:
-                if hour == 8:
-                    period = "3"
-                elif hour == 9:
-                    if minute <= 50:
-                        period = "3"
-                    elif minute > 50:
-                        period = "4"
-                elif hour == 10:
-                    period = "4"
-                elif hour == 11:
-                    if minute <= 20:
-                         period = "4"
-                    elif minute > 20:
+                    elif hour == 11:
+                        if minute <= 20:
+                            period = "4"
+                        elif minute > 20:
+                            period = "2"
+                    elif hour == 12:
+                        if minute == 0:
+                            period = "2"
+                        elif minute > 20:
+                            period = "1"
+                    elif hour == 13:
+                        if minute <= 5:
+                            period = "1"
+                        elif minute > 5 and minute <= 45:
+                            period = "2"
+                if weekday == 3:
+                    if hour == 8:
+                        period = "1"
+                    elif hour == 9:
+                        if minute <= 50:
+                            period = "1"
+                        elif minute > 50:
+                            period = "2"
+                    elif hour == 10:
                         period = "2"
-                elif hour == 12:
-                    if minute == 0:
-                        period = "2"
-                    elif minute > 20:
-                        period = "1"
-                elif hour == 13:
-                    if minute <= 5:
-                        period = "1"
-                    elif minute > 5 and minute <= 45:
-                        period = "2"
-            if weekday == 3:
-                if hour == 8:
-                    period = "1"
-                elif hour == 9:
-                    if minute <= 50:
-                        period = "1"
-                    elif minute > 50:
-                        period = "2"
-                elif hour == 10:
-                    period = "2"
-                elif hour == 11:
-                    if minute <= 20:
-                         period = "2"
-                    elif minute > 20:
+                    elif hour == 11:
+                        if minute <= 20:
+                            period = "2"
+                        elif minute > 20:
+                            period = "3"
+                    elif hour == 12:
+                        if minute == 0:
+                            period = "3"
+                        elif minute > 20:
+                            period = "3"
+                    elif hour == 13:
+                        if minute <= 5:
+                            period = "3"
+                        elif minute > 5 and minute <= 45:
+                            period = "4"
+                if weekday == 4:
+                    if hour == 8:
                         period = "3"
-                elif hour == 12:
-                    if minute == 0:
-                        period = "3"
-                    elif minute > 20:
-                        period = "3"
-                elif hour == 13:
-                    if minute <= 5:
-                        period = "3"
-                    elif minute > 5 and minute <= 45:
+                    elif hour == 9:
+                        if minute <= 50:
+                            period = "3"
+                        elif minute > 50:
+                            period = "4"
+                    elif hour == 10:
                         period = "4"
-            if weekday == 4:
-                if hour == 8:
-                    period = "3"
-                elif hour == 9:
-                    if minute <= 50:
-                        period = "3"
-                    elif minute > 50:
-                        period = "4"
-                elif hour == 10:
-                    period = "4"
-                elif hour == 11:
-                    if minute <= 20:
-                         period = "4"
-                    elif minute > 20:
-                        period = "4"
-                elif hour == 12:
-                    if minute == 0:
-                        period = "4"
-                    elif minute > 20:
-                        period = "1"
-                elif hour == 13:
-                    if minute <= 5:
-                        period = "1"
-                    elif minute > 5 and minute <= 45:
-                        period = "2"
-            if weekday == 2:
-                if hour == 9:
-                    if minute > 50:
-                        period = "1"
-                elif hour == 10:
-                    if minute <= 35:
-                        period = "1"
-                    elif minute > 35:
-                        period = "2"
-                elif hour == 11:
-                    if minute <= 20:
-                         period = "2"
-                elif hour == 12:
-                    if minute > 20:
-                        period = "3"
-                elif hour == 13:
-                    if minute <= 5:
-                        period = "3"
-                    elif minute > 5 and minute <= 45:
-                        period = "4"
+                    elif hour == 11:
+                        if minute <= 20:
+                            period = "4"
+                        elif minute > 20:
+                            period = "4"
+                    elif hour == 12:
+                        if minute == 0:
+                            period = "4"
+                        elif minute > 20:
+                            period = "1"
+                    elif hour == 13:
+                        if minute <= 5:
+                            period = "1"
+                        elif minute > 5 and minute <= 45:
+                            period = "2"
+                if weekday == 2:
+                    if hour == 9:
+                        if minute > 50:
+                            period = "1"
+                    elif hour == 10:
+                        if minute <= 35:
+                            period = "1"
+                        elif minute > 35:
+                            period = "2"
+                    elif hour == 11:
+                        if minute <= 20:
+                            period = "2"
+                    elif hour == 12:
+                        if minute > 20:
+                            period = "3"
+                    elif hour == 13:
+                        if minute <= 5:
+                            period = "3"
+                        elif minute > 5 and minute <= 45:
+                            period = "4"
+            if ib:
+                period == "8"
             if period == "0":
                 await message.channel.send(f"{message.author.mention}, you do not have any classes right now")
+            elif period == "8":
+                await message.channel.send(f"{message.author.mention}, we do not have IB now functionality yet. For now please use list instead. @abdeet if you have a copy of the schedule so abhi can implement now")
             else:
                 nowembed = discord.Embed(title = f"**Class right now for {message.author.name}**", description = f"[{userdata['names'][period]}]({userdata['links'][period]})")
                 await message.channel.send(embed = nowembed)
